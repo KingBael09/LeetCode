28fd91726c9c9fa99d2ad854d550ad32b785356c
KingBael <www.jayesh567@gmail.com>
2023-08-11 22:30:37 +0530

    two pointer method

---
+++ 1679.cpp
@@ -0,0 +1,46 @@
+// ? 1679. Max Number of K-Sum Pairs
+// ? https://leetcode.com/problems/max-number-of-k-sum-pairs/description/
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxOperations(vector<int> &nums, int k)
+{
+    sort(nums.begin(), nums.end());
+
+    int start = 0;
+    int end = nums.size() - 1;
+    int opr = 0;
+    while (start < end)
+    {
+        int curSum = nums[start] + nums[end];
+        if (curSum > k)
+        {
+            end--;
+        }
+        else if (curSum < k)
+        {
+            start++;
+        }
+        else if (curSum == k)
+        {
+            opr++;
+            start++;
+            end--;
+        }
+    }
+
+    return opr;
+}
+
+int main()
+{
+    vector<int> list{1, 2, 3, 4};
+    int target = 5;
+
+    int ans = maxOperations(list, target);
+
+    cout << ans << endl;
+}
