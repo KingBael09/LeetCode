892b129d80656f5e2b278b6135b108255b04d328
KingBael <www.jayesh567@gmail.com>
2023-09-12 00:13:32 +0530

    solved

--- 0326.cpp
+++ 0326.cpp
@@ -7,8 +7,10 @@
 
 using namespace std;
 
+// recursion was not allowed
 bool isPowerOfThree(int n)
 {
+    log10(n);
     if (n <= 0)
     {
         return false;
@@ -25,6 +27,25 @@ bool isPowerOfThree(int n)
     }
 
     return false;
+}
+
+bool betterSOl(int n)
+{
+    if (n <= 0)
+    {
+        return false;
+    }
+
+    double l = log10(n) / log10(3);
+    int i = l;
+
+    return l - i == 0;
+}
+
+// This is too genius of a solution its -> 3^19 = 1162261467
+bool tooGeniusMethod(int n)
+{
+    return n > 0 && 1162261467 % n == 0;
 }
 
 int main()
