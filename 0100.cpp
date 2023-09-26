de2fa46afe5f7bd2535af5aa90753354f8d26ce3
KingBael <www.jayesh567@gmail.com>
2023-09-26 22:02:15 +0530

    solved

---
+++ 0100.cpp
@@ -0,0 +1,38 @@
+// ? 100. Same Tree
+// ? https://leetcode.com/problems/same-tree/description/
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool isSameTree(TreeNode *p, TreeNode *q)
+{
+    if (p == nullptr && q == nullptr)
+    {
+        return true;
+    }
+
+    if (p && q && p->val == q->val)
+    {
+        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
+    }
+
+    return false;
+}
+
+int main()
+{
+    // too lazy to write driver code
+}
