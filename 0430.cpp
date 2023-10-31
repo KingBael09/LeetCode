eb5dc7a985955e3e4d822fa572f58b2d5fc144d1
KingBael <www.jayesh567@gmail.com>
2023-10-31 23:47:37 +0530

    solved

--- 0430.cpp
+++ 0430.cpp
@@ -49,6 +49,47 @@ Node *flatten(Node *head)
     return head;
 }
 
+Node *flattenBetter(Node *head)
+{
+    auto curr = head;
+    while (curr)
+    {
+        if (!curr->child)
+        {
+            curr = curr->next;
+            continue;
+        }
+
+        auto tail = curr->child;
+
+        auto look_out = curr;
+        // lookout allows us to jump without re-traversion to nodes with children
+
+        while (tail->next)
+        {
+            if (tail->child)
+            {
+                look_out = tail;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = curr->next;
+
+        if (curr->next)
+        {
+            curr->next->prev = tail;
+        }
+
+        curr->next = curr->child;
+        curr->child->prev = curr;
+        curr->child = nullptr;
+        curr = look_out;
+    }
+
+    return head;
+}
+
 int main()
 {
 }
