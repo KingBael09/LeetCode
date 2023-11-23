fd58960174129a5ea6dfe9bb4c7f09a2c16ff612
KingBael <www.jayesh567@gmail.com>
2023-11-23 16:47:42 +0530

    solved

---
+++ 0175.sql
@@ -0,0 +1,9 @@
+-- ? 175. Combine Two Tables
+-- ? https://leetcode.com/problems/combine-two-tables/description/
+-- 
+SELECT P.firstName,
+    P.lastName,
+    A.city,
+    A.state
+FROM Person P
+    LEFT JOIN Address A ON P.personId = A.personId
