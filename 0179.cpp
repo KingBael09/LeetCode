9a67f4317bcec6aa806ef6e1c9d93af0dca992a3
KingBael <www.jayesh567@gmail.com>
2023-11-26 19:15:39 +0530

    solved

---
+++ 0179.cpp
@@ -0,0 +1,44 @@
+// ? 179. Largest Number
+// ? https://leetcode.com/problems/largest-number/description/
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+static bool compare(string &a, string &b)
+{
+    return a + b > b + a;
+}
+
+string largestNumber(vector<int> &nums)
+{
+    string ans = "";
+
+    vector<string> strs;
+
+    for (auto &i : nums)
+    {
+        strs.push_back(to_string(i));
+    }
+
+    sort(strs.begin(), strs.end(), compare);
+
+    for (auto &i : strs)
+    {
+        ans += i;
+    }
+
+    // For test case [0,0] -> "00" -> "0"
+    if (ans.front() == '0')
+    {
+        return "0";
+    }
+
+    return ans;
+}
+
+int main()
+{
+}
