cfdaad685404de7a02ee7aef4acddd5b83b56d5b
KingBael <www.jayesh567@gmail.com>
2023-12-18 23:00:56 +0530

    solved

---
+++ 1050.sql
@@ -0,0 +1,9 @@
+-- ? 1050. Actors and Directors Who Cooperated At Least Three Times
+-- ? https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/description/
+-- 
+SELECT actor_id,
+    director_id
+FROM ActorDirector
+GROUP BY actor_id,
+    director_id
+HAVING COUNT(timestamp) >= 3;
