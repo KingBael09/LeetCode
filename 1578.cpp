098ab2d31f9dad293b58e33cdd6488252ad22758
KingBael <www.jayesh567@gmail.com>
2023-12-27 19:41:25 +0530

    solved

---
+++ 1578.cpp
@@ -0,0 +1,37 @@
+// ? 1578. Minimum Time to Make Rope Colorful
+// ? https://leetcode.com/problems/minimum-time-to-make-rope-colorful/description/
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int minCost(string colors, vector<int> &neededTime)
+{
+    int prev = 0;
+    int time = 0;
+
+    for (int next = 1; next < colors.size(); next++)
+    {
+        if (colors[prev] != colors[next])
+        {
+            prev = next;
+            continue;
+        }
+
+        time += min(neededTime[prev], neededTime[next]);
+
+        if (neededTime[prev] < neededTime[next])
+        {
+            prev = next;
+            // match prev only if we select previous
+        }
+    }
+
+    return time;
+}
+
+int main()
+{
+}
