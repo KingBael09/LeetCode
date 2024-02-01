667f5ce5bd43dff83c670f82e7337f75feeba886
KingBael <www.jayesh567@gmail.com>
2024-02-02 00:21:52 +0530

    solved

--- src/bin/0383.rs
+++ src/bin/0383.rs
@@ -1,25 +1,14 @@
 use std::collections::HashMap;
 
 pub fn can_construct(ransom_note: String, magazine: String) -> bool {
-    if magazine.len() < ransom_note.len() {
-        return false;
-    }
-
     let mut map = HashMap::new();
 
-    for c in magazine.chars() {
-        *map.entry(c).or_insert(0) += 1;
-    }
+    magazine.chars().for_each(|ch| {
+        map.entry(ch).and_modify(|ct| *ct += 1).or_insert(1);
+    });
 
-    for c in ransom_note.chars() {
-        let count = map.entry(c).or_insert(0);
-        *count -= 1;
-
-        if *count < 0 {
-            return false;
-        }
-    }
-
-    true
+    ransom_note
+        .chars()
+        .all(|ch| map.entry(ch).and_modify(|ct| *ct -= 1).or_insert(-1) >= &mut 0)
 }
 fn main() {}
