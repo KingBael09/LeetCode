46cae33209fdc6ddec3541ad98df4294ce12997e
KingBael <www.jayesh567@gmail.com>
2024-02-05 23:42:42 +0530

    solved

--- src/bin/0875.rs
+++ src/bin/0875.rs
@@ -28,19 +28,18 @@ pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
 }
 
 pub fn min_eating_speed_smaller(piles: Vec<i32>, h: i32) -> i32 {
-    let mut left = 1;
-    let mut right = *piles.iter().reduce(|f, c| f.max(c)).unwrap();
+    let (mut left, mut right) = (1, *piles.iter().max().unwrap());
 
     let mut ans = right;
 
     while left <= right {
         let mid = left + (right - left) / 2;
 
-        let hr = piles.iter().fold(0_i64, |total, bananas| {
-            total + ((bananas + mid - 1) / mid) as i64
+        let hr = piles.iter().fold(0_i32, |total, bananas| {
+            total.saturating_add((bananas + mid - 1) / mid)
         });
 
-        match hr.cmp(&(h as i64)) {
+        match hr.cmp(&h) {
             Ordering::Greater => left = mid + 1,
             _ => {
                 ans = cmp::min(ans, mid);
