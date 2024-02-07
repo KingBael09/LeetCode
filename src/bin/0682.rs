19f955388e7c17e4595b708ab2bb42592fc0ea5b
KingBael <www.jayesh567@gmail.com>
2024-02-07 23:47:53 +0530

    solved

---
+++ src/bin/0682.rs
@@ -0,0 +1,16 @@
+pub fn cal_points(operations: Vec<String>) -> i32 {
+    let mut res: Vec<i32> = Vec::new();
+
+    for str in operations.iter() {
+        match str.as_str() {
+            "C" => res.truncate(res.len() - 1),
+            "D" => res.push(res.last().take().unwrap() * 2),
+            "+" => res.push(res.iter().rev().take(2).sum()),
+            s => res.push(s.parse().unwrap()),
+        }
+    }
+
+    res.into_iter().sum()
+}
+
+fn main() {}
