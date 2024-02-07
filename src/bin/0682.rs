c34ffcfeac5e398097668c8c6a6cec7db5b300c4
KingBael <www.jayesh567@gmail.com>
2024-02-08 00:13:40 +0530

    solved

--- src/bin/0682.rs
+++ src/bin/0682.rs
@@ -1,16 +1,28 @@
 pub fn cal_points(operations: Vec<String>) -> i32 {
-    let mut res = Vec::new();
+    operations
+        .into_iter()
+        .fold(vec![], |mut st, str| {
+            match str.as_str() {
+                "C" => st.truncate(st.len() - 1),
+                "D" => st.push(st.last().take().unwrap() * 2),
+                "+" => st.push(st.iter().rev().take(2).sum()),
+                s => st.push(s.parse().unwrap()),
+            };
+            st
+        })
+        .into_iter()
+        .sum()
 
-    for str in operations.iter() {
-        match str.as_str() {
-            "C" => res.truncate(res.len() - 1),
-            "D" => res.push(res.last().take().unwrap() * 2),
-            "+" => res.push(res.iter().rev().take(2).sum()),
-            s => res.push(s.parse().unwrap()),
-        }
-    }
+    // let mut res = Vec::new();
 
-    res.into_iter().sum()
+    // operations.into_iter().for_each(|str| match str.as_str() {
+    //     "C" => res.truncate(res.len() - 1),
+    //     "D" => res.push(res.last().take().unwrap() * 2),
+    //     "+" => res.push(res.iter().rev().take(2).sum()),
+    //     s => res.push(s.parse().unwrap()),
+    // });
+
+    // res.into_iter().sum()
 }
 
 fn main() {}
