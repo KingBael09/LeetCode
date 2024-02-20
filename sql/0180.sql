a8a0282355343b890e469c4e683e5a3d2d218dd6
KingBael <www.jayesh567@gmail.com>
2024-02-20 23:13:37 +0530

    solved

---
+++ sql/0180.sql
@@ -0,0 +1,19 @@
+select distinct l1.num ConsecutiveNums
+from logs l1,
+    logs l2,
+    logs l3
+where l1.num = l2.num
+    and l1.num = l3.num
+    and l2.id = l1.id + 1
+    and l3.id = l1.id + 2;
+;
+;
+;
+-- Other solution
+;
+select distinct a.num ConsecutiveNums
+from logs a
+    join logs b on a.id + 1 = b.id
+    join logs c on b.id + 1 = c.id
+where a.num = b.num
+    and c.num = b.num
